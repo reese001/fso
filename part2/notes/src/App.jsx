cedd0283c11506f96d3bb5e5e767410a392b4b3d
Reese Parsons <reeseparsons99@gmail.com>
2025-11-11 21:22:21 -0400

    update notes to use services directory

--- part2/notes/src/App.jsx
+++ part2/notes/src/App.jsx
@@ -1,6 +1,6 @@
 import Note from './components/Note'
 import { useState, useEffect } from 'react'
-import axios from 'axios'
+import noteService from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([]) 
@@ -8,15 +8,12 @@ const App = () => {
   const [showAll, setShowAll] = useState(true)
 
   useEffect(() => {
-    console.log('effect')
-    axios.
-    get('http://localhost:3001/notes')
-    .then(response => {
-      console.log(response)
-      setNotes(response.data)
+    noteService
+    .getAll()
+    .then(initialNotes => {
+      setNotes(initialNotes) 
     })
   }, [])
-   console.log('render', notes.length, 'notes')
 
   const handleNoteChange = (event) => {
     console.log(event.target.value)
@@ -25,7 +22,26 @@ const App = () => {
 
   const addNote = (event) => {
     event.preventDefault()
-    console.log('button clicked', event.target)
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5
+    }
+    noteService
+    .create(noteObject)
+    .then(returnedNote => {
+      setNotes(notes.concat(returnedNote))
+      setNewNote('')
+    })
+  }
+
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = {...note, important: !note.important}
+    noteService
+    .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id === id ? returnedNote : note))
+      })
   }
 
   const notesToShow = showAll ? notes : notes.filter(note => note.important)
@@ -40,7 +56,7 @@ const App = () => {
       </div>
       <ul>
         {notesToShow.map(note => 
-          <Note key={note.id} note={note} />
+          <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}/>
         )}
       </ul>
       <form onSubmit={addNote}>
