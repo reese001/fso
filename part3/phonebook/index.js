674fea77f0a9e4662b1610dddb53f5a6fb7f2568
Reese Parsons <reeseparsons99@gmail.com>
2025-11-13 11:56:05 -0400

    part 3 exercises 3.7-3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,28 +1,34 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+morgan.token('body', req => {
+    return JSON.stringify(req.body)
+  })
+  
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
+        "id": "1",
+        "name": "Arto Hellas", 
+        "number": "040-123456"
     },
     { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
+        "id": "2",
+        "name": "Ada Lovelace", 
+        "number": "39-44-5323523"
     },
     { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
+        "id": "3",
+        "name": "Dan Abramov", 
+        "number": "12-43-234345"
     },
     { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
+        "id": "4",
+        "name": "Mary Poppendieck", 
+        "number": "39-23-6423122"
     }
 ]
 
