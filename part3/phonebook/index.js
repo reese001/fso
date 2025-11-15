ce899bd2b950ff6aea703ab51ee703134fbbd2c3
Reese Parsons <reeseparsons99@gmail.com>
2025-11-15 16:42:25 -0400

    Refactor backend to use Mongoose for database operations and remove deprecated mongo.js files. Update phonebook API to utilize Mongoose for person data retrieval and saving, and add dotenv for environment variable management.

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+require('dotenv').config()
+const Person = require('./models/person')
 
 app.use(express.json())
 morgan.token('body', req => {
@@ -9,32 +11,12 @@ morgan.token('body', req => {
   
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    { 
-        "id": "1",
-        "name": "Arto Hellas", 
-        "number": "040-123456"
-    },
-    { 
-        "id": "2",
-        "name": "Ada Lovelace", 
-        "number": "39-44-5323523"
-    },
-    { 
-        "id": "3",
-        "name": "Dan Abramov", 
-        "number": "12-43-234345"
-    },
-    { 
-        "id": "4",
-        "name": "Mary Poppendieck", 
-        "number": "39-23-6423122"
-    }
-]
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
-  })
+    Person.find({}).then(persons => {
+        response.json(persons)
+    })
+})
 
 app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
@@ -58,26 +40,46 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+// app.post('/api/notes', (request, response) => {
+//   const body = request.body
+
+//   if (!body.content) {
+//     return response.status(400).json({ 
+//       error: 'content missing' 
+//     })
+//   }
+
+//   const note = new Note({
+//     content: body.content,
+//     important: body.important || false,
+//   })
+
+//   note.save().then(savedNote => {
+//     response.json(savedNote)
+//   })
+// })
+
+    
+
 app.post('/api/persons', (request, response) => {
     const body = request.body
     if (!body.name || !body.number) {
         return response.status(400).json({
             error: "name and number required"
         })
-    } else if (persons.some(person => person.name === body.name)) {
-        return response.status(409).json({
-            error: "name must be unique"
-        })
-    }
-    const person = {
-        id: String(Math.floor(Math.random() * 9999999999)),
+    } 
+    const person = new Person({
+        id: Math.floor(Math.random() * 99999999999),
         name: body.name,
-        number: body.number,
-    }
-    persons = persons.concat(person)
-
-    response.json(person)
+        number: body.number
+    })
+    
+    person.save().then(savedPerson => {
+        response.json(savedPerson)
+    })
 })
+
+    
 
 
 const PORT = 3001
